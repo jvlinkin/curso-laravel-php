57f748f9a6b0495c7f2302d9e2c5c61933f13433
joaopaiva <joao.paiva@dbm.com.br>
2023-02-16 15:04:30 -0300

    feat: trazendo usuarios do banco, e renderizando na view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        $users = User::all();
+
+        return view('home/index', [
+            'users' => $users
+        ]);
+    }
+}
